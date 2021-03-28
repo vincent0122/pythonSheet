189dfe890fc3bddf00ea8f81edc446092dd5db9c
vinc <38850727+vincent0122@users.noreply.github.com>
2021-03-28 23:39:19 +0900

    change allowd host

--- users/views.py
+++ users/views.py
@@ -62,7 +62,7 @@ def complete_verification(request, key):
 
 def kakao_login(request):
     client_id = os.environ.get("KAKAO_ID")
-    redirect_uri = " https://hpdjango.herokuapp.com/users/login/kakao/callback"
+    redirect_uri = "https://hpdjango.herokuapp.com/users/login/kakao/callback"
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
     )
@@ -76,7 +76,7 @@ def kakao_callback(request):
     try:
         code = request.GET.get("code")
         client_id = os.environ.get("KAKAO_ID")
-        redirect_uri = " https://hpdjango.herokuapp.com/users/login/kakao/callback"
+        redirect_uri = "https://hpdjango.herokuapp.com/users/login/kakao/callback"
         token_request = requests.get(
             f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={redirect_uri}&code={code}"
         )
