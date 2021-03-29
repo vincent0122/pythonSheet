e54ade5a6a6f6af7f58dc0233bed977a0d2556c7
vinc <38850727+vincent0122@users.noreply.github.com>
2021-03-29 09:39:07 +0900

    display file_urls in 내용

--- issues/views.py
+++ issues/views.py
@@ -69,21 +69,22 @@ def issue_create(request):
 
     print(file_urls)
 
-    airtable.insert(
-        {
-            "작성자": name,
-            "거래처": customer,
-            "내용": file_urls,
-            # "Attachments": [
-            #     {
-            #         "url": "https://2.bp.blogspot.com/-aaYab7phjF4/Xa_TOXvC5hI/AAAAAAAAQME/2xf9AYY0450n-hAobHdEHRrYmPbcy0jsACLcBGAsYHQ/w914-h514-p-k-no-nu/suzy-beautiful-korean-girl-uhdpaper.com-4K-4.1423-wp.thumbnail.jpg"
-            #     },
-            #     {
-            #         "url": "https://dl.airtable.com/.attachments/9d020ee5ca79c9b527c030ced8d83bef/0d11df0c/KakaoTalk_20200527_084727449.png",
-            #     },
-            # ],
-        }
-    )
+    if issue:
+        airtable.insert(
+            {
+                "작성자": name,
+                "거래처": customer,
+                "내용": file_urls,
+                # "Attachments": [
+                #     {
+                #         "url": "https://2.bp.blogspot.com/-aaYab7phjF4/Xa_TOXvC5hI/AAAAAAAAQME/2xf9AYY0450n-hAobHdEHRrYmPbcy0jsACLcBGAsYHQ/w914-h514-p-k-no-nu/suzy-beautiful-korean-girl-uhdpaper.com-4K-4.1423-wp.thumbnail.jpg"
+                #     },
+                #     {
+                #         "url": "https://dl.airtable.com/.attachments/9d020ee5ca79c9b527c030ced8d83bef/0d11df0c/KakaoTalk_20200527_084727449.png",
+                #     },
+                # ],
+            }
+        )
 
     # 에어테이블 업로드 후, 삭제
 
