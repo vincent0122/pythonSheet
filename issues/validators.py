0414a737350825d05edc0b9c089e26610ca14d92
vinc <38850727+vincent0122@users.noreply.github.com>
2021-03-29 13:59:21 +0900

    set maxfile size as 30mb

--- issues/validators.py
+++ issues/validators.py
@@ -3,9 +3,9 @@ from django.core.exceptions import ValidationError
 
 def validate_file_size(value):
     filesize = value.size
-    limit = 50 * 1024 * 1024
+    limit = 30 * 1024 * 1024
 
     if filesize > limit:
-        raise ValidationError("The maximum file size that can be uploaded is 10MB")
+        raise ValidationError("The maximum file size that can be uploaded is 30MB")
     else:
         return value
