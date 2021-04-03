22f044572e7aac9bcfd59690a32621e95624324b
vinc <38850727+vincent0122@users.noreply.github.com>
2021-04-03 23:03:22 +0900

    nono

--- stocks/apps.py
+++ stocks/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from .google_auth import get_google
-from backdata import basic_setting
+
+# from .backdata import basic_setting
 import pandas as pd
 
 from datetime import timedelta
@@ -14,6 +15,55 @@ import plotly.graph_objects as go
 # from plotly.graph_objs import *
 
 # import json
+basic_setting = {
+    "items": [
+        "다바오DC",
+        "DC BROWN(로버트)",
+        "코코맥스(프랭클린)",
+        "코코맥스(INHILL)(52)",
+        "코코맥스(P)",
+        "코코맥스30",
+        "코코맥스(W50)",
+        "페어링파우더(FB)",
+        "COCOP",
+        "COCOR",
+        "코코맥스(ROYAL)",
+        "다바오 LOW FAT",
+        "맥주효모(중국)",
+        "맥주효모(베트남)",
+        "대두박",
+        "젤라틴(고)",
+        "젤라틴(중)",
+        "젤라틴(저)",
+        "변성 타피오카",
+        "바나나분말(톤백)",
+        "바나나분말(지대)",
+        "위너",
+        "락토젠",
+        "그로우어",
+        "미라클",
+        "구연산(지대)",
+        "솔빈산칼륨(입자)",
+        "솔빈산칼륨(가루)",
+        "프로피온산칼슘(입자)",
+        "프로틴파워(소이코밀20)",
+        "소이코밀",
+        "ISP",
+        "멀티락",
+        "멀티락(新)",
+        "씨센스 프리미엄",
+        "디텍",
+        "디텍(에이티바이오)",
+        "케르세틴",
+        "렌틸콩(톤백)",
+        "렌틸콩(지대)",
+        "커피화분(허니텍)",
+        "인도화분",
+        "CLA(유대표님 보관대행)",
+        "바이오스플린트(유대표님 보관대행)",
+    ],
+    "period_to_check": 60,
+}
 
 
 class StocksConfig(AppConfig):
