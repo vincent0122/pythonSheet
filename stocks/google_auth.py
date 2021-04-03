6c9e8a048be615323bd5ae8e05d8298b02c26590
vinc <38850727+vincent0122@users.noreply.github.com>
2021-04-03 23:45:13 +0900

    nono

--- stocks/google_auth.py
+++ stocks/google_auth.py
@@ -1,14 +1,17 @@
 import gspread
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 def get_google():
+    json = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
     scope = [
         "https://spreadsheets.google.com/feeds",
         "https://www.googleapis.com/auth/drive",
     ]
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        "./creden/google-credentials.json", scope
-    )  # Your json file here
+    # credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    #     "./creden/google-credentials.json", scope
+    # )  # Your json file here
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(json, scope)
     gc = gspread.authorize(credentials)
     return gc
