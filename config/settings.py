acad42bb5a9bf2f5282e85c296236e88050bf75a
vinc <38850727+vincent0122@users.noreply.github.com>
2021-04-06 13:43:31 +0900

    clear db

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "hpdjango.herokuapp.com"]
 
@@ -62,14 +62,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-DATABASES2 = {
+DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
-DATABASES = {
+DATABASES2 = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "djangogirls",
