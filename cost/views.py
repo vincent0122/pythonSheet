88fcb63547154a30a20bd50652829a0574b450ec
vinc <38850727+vincent0122@users.noreply.github.com>
2021-04-07 10:20:00 +0900

    all

--- cost/views.py
+++ cost/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .apps import update_cell
+from users import models as user_models
+import time
 
 # Create your views here.
 
 
 def cost_update(request):
-    update_cell()
+    user = request.user
+    when = time.strftime("%y-%m-%d %H:%M:%S")
+    amount = request.GET.get("amount")
+    detail = request.GET.get("detail")
+    etc = request.GET.get("etc")
+
+    user = user_models.User.objects.get(email=request.user).first_name
+
+    update_cell(when, user, amount, detail, etc)
     return render(request, "cost/update.html")
