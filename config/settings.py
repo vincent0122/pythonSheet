e8687b22d2bc34d6a5ff403252d4d55a806e333f
vinc <38850727+vincent0122@users.noreply.github.com>
2021-04-12 10:05:05 +0900

    debug False

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "hpdjango.herokuapp.com", "hpdjangostaging.herokuapp.com"]
 
